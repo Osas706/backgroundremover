34d5011c5b4960074aae75ff20b75cbb94cf1f81
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-02-16 13:21:29 +0100

    ..

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 
+      style={{
+        fontSize: '35px',
+        textAlign: 'center',
+        background: 'transparent',
+        color: `white`,
+        margin: '30px auto'
+        
+      }}
+    >
+        Remove the <span style={{
+            background: 'linear-gradient(to right, #eee, purple)',
+            webkitBackgroundClip: 'text',
+           webkitTextFillColor: 'transparent',
+        }}>background</span> of any image in 3,2,1....
+    </h1>
+  )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "../index.css";
+
+const Main = () => {
+  const [selected, setSelected] = useState(null);
+  const [finalImage, setFinalImage] = useState(null);
+
+  const handleFileInputChange = (e) => {
+    let image = e.target.files?.[0];
+    setSelected(image);
+  };
+
+  const handleFileUpload = async () => {
+    const formData = new FormData();
+    formData.append("image_file", selected);
+    formData.append("size", "auto");
+
+    const api_key = "uTAsGGX3HY9BwxSpeBupVFZn";
+
+    // send to the server
+    try {
+      const response = await axios.post(
+        "https://api.remove.bg/v1.0/removebg",
+        formData,
+        {
+          headers: {
+            "X-Api-Key": api_key,
+          },
+          responseType: "blob",
+        }
+      );
+
+      const url = URL.createObjectURL(response.data);
+      setFinalImage(url);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    } finally {
+    }
+  };
+
+  return (
+    <div className="container">
+      
+        <div className="input-div">
+          <img src={selected ? URL.createObjectURL(selected) : ""} />
+
+          <div className="file-section">
+            <input type="file" name="" id="" onChange={handleFileInputChange} />
+            <button onClick={handleFileUpload}>Upload</button>
+          </div>
+        </div>
+ 
+
+      
+        <div className="input-div">
+          <img src={finalImage} />
+          <a href={finalImage} download={"save.png"} className="download-btn">
+            <button className="download-btn">Download Image</button>
+          </a>
+        </div>
+
+    </div>
+  );
+};
+
+export default Main;
